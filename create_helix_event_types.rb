194f002a71dac0eb4908287dbeaa9a6e90dcd088
Connor Rhodes <connor@rhodes.contact>
2024-12-22 21:02:28 -0600

    add initial create_helix_event_types script

---
+++ create_helix_event_types.rb
@@ -0,0 +1,7 @@
+require_relative 'lib/load_config_csv'
+require_relative 'lib/load_event_types_config'
+require 'pry'
+
+event_types_config = load_event_types_config('event_types_config.csv')
+
+binding.pry

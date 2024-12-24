a137b96dbd60bc67a87e7a73f760f7ce0cf59f3b
Connor Rhodes <connor@rhodes.contact>
2024-12-23 20:50:01 -0600

    update post by keyid route to infer helix event type from provided mappings in the request body

--- server.rb
+++ server.rb
@@ -28,13 +28,32 @@ end
 
 post '/event/keyid' do
   body = JSON.parse(request.body.read)
-	binding.pry
+	event_types_config.each do |event_type, mappings|
+    # Find the event type mapping in this group
+    event_type_mapping = mappings.find { |mapping| mapping[:data_purpose] == "event type id" }
+    
+    next unless event_type_mapping
+
+		id_key = {}
+		id_key[event_type_mapping[:remote_key]] = event_type_mapping[:helix_key]
+
+		if body[id_key.keys.first] == id_key.values.first
+			# TODO: add logic to generate payload for helix event
+			puts "Found event type mapping for #{event_type}"
+		end
+  end
+	return "Under Construction"
 	# result = VAPI.create_helix_event(
 	# 	event_type_uid: nil,
 	# 	camera_id: nil,
 	# 	attributes: nil,
 	# 	time: nil
 	# )
+end
+
+post '/debug' do
+  body = JSON.parse(request.body.read)
+	"test"
 end
 
 post '/event/deviceid' do

39f469b30c94abaccd5ffebd0463eb74b78d9e50
Connor Rhodes <connor@rhodes.contact>
2024-12-23 19:45:38 -0600

    update endpoint

--- tests/post-json.rb
+++ tests/post-json.rb
@@ -9,7 +9,7 @@ body = {
 }
 
 response = hp.post(
-  'http://localhost:8080/event',
+  'http://localhost:8080/event/keyid',
   headers: { 'Content-Type' => 'application/json' },
   body: body.to_json
 )

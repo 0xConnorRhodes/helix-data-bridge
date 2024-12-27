639bf6eff8c6f5cb878d3efae50bd556290b3f1c
Connor Rhodes <connor@rhodes.contact>
2024-12-27 12:08:14 -0600

    added setup script on ubuntu to install ruby

---
+++ deploy/setup-ubuntu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install -y \
+  autoconf \
+  patch \
+  build-essential \
+  rustc \
+  libssl-dev \
+  libyaml-dev \
+  libreadline6-dev \
+  zlib1g-dev \
+  libgmp-dev \
+  libncurses5-dev \
+  libffi-dev \
+  libgdbm6 \
+  libgdbm-dev \
+  libdb-dev \
+  uuid-dev
+
+curl https://mise.run | MISE_VERSION=v2024.12.20 sh
+
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+eval "$(~/.local/bin/mise activate bash)"
+
+mise use --global ruby@3.3.6
+
+exec bash # reload bash to apply mise activation

f948759f90bda50d5feda7db75b4f852e2d653f5
Connor Rhodes <connor@rhodes.contact>
2025-01-18 16:58:32 -0600

    simplify create_helix_event_types and move to lib

---
+++ lib/create_event_types.rb
@@ -0,0 +1,31 @@
+def create_event_types(config)
+  data_purpose_check = check_data_purpose_field(config)
+  exit(1) unless data_purpose_check
+  
+  # remove fields from config that are not used in the Helix schema
+  stripped_config = config.transform_values do |mappings|
+    mappings
+      .reject { |mapping| mapping[:data_purpose] == "event type id" }
+      .map { |mapping| mapping.reject { |k, v| v.nil? || k == :data_purpose } }
+  end
+  
+  stripped_config.each do |event|
+  	schema = event[1].each_with_object({}) do |mapping, schema|
+  		schema[mapping[:helix_key]] = mapping[:data_type]
+  	end
+  
+    begin
+      result = $vapi.create_helix_event_type(
+        name: event[0],
+        schema: schema
+      )
+    rescue => e
+      if e.message.include?("already exists for organization")
+        # TODO: display error HTML page & ask user to manually delete or edit event type in Command
+        puts "Event type \"#{event[0]}\" already exists"
+      else
+        raise e
+      end
+    end
+  end
+end
